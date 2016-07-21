ed378af81fdc3f133c0f270d8d08affb013451c7
alka-axonic <alexkaefer@gmail.com>
2016-07-21 14:29:12 +0200

    only one instance

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 
 app.get('/', function (req, res) {
     //res.send(JSON.parse(shelljs.exec('aws ec2 describe-instances --filters Name=tag:NODE_ENV,Values=production')));
-    res.send(JSON.parse(shelljs.exec('aws ec2 describe-instances')));
+    var privateIp = process.env.HOSTNAME.replace('ip-', '');
+    res.send(JSON.parse(shelljs.exec('aws ec2 describe-instances --filters Name=private-ip-address,Values='+privateIp)));//aws ec2 describe-instances --filters Name=private-ip-address,Values=
     res.send('Hello World! from AWS!!! UHUHUU');
 });
 
