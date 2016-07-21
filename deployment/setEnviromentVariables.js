bb58e4fa5e0c1200ec1127ba9fbfe5b23d018f74
alka-axonic <alexkaefer@gmail.com>
2016-07-21 15:48:24 +0200

    dsafdsa

--- deployment/setEnviromentVariables.js
+++ deployment/setEnviromentVariables.js
@@ -1,9 +1,12 @@
 var async       = require('async');
 var fs          = require('fs');
+var os          = require('os');
 var shelljs     = require('shelljs');
+var validateip  = require('validate-ip');
 
-async.auto({
+/*async.auto({
     getRDSinstanceData : function(callback, results){
+        // --db-instance-identifier muss gleich DB Instance sein.
         shelljs.exec('aws rds describe-db-instances --db-instance-identifier ' + process.env.NODE_ENV + ' > /home/ec2-user/rds.json', function(code, stdout, stderr){
             if ( stderr ) return callback(stderr);
 
@@ -12,7 +15,7 @@ async.auto({
     },
     readRDSDatafromFile : ['getRDSinstanceData', function(callback, results) {
         if ( results.getRDSinstanceData ){
-            fs.readFile('/home/ec2-user/rds.jsonX', 'utf8', function (err, data) {
+            fs.readFile('/home/ec2-user/rds.json', 'utf8', function (err, data) {
                 if ( err ) return callback(err);
                 var obj = JSON.parse(data);
                 return callback(null, obj);
@@ -28,5 +31,48 @@ async.auto({
 }, function(err, results){
     if ( err ) return console.log(err);
     console.log(results);
-});
+});*/
+
+var Controller = {
+    setENviromentVariables: function(next){
+        async.auto({
+            extractInstancePrivateIp    : function(callback, result) {
+                var privateIp = os.hostname().replace('ip-', '').replace(/-/g,'.');
+                if ( !validateip(privateIp) ) return callback('Invalid IP');
+                callback(null, privateIp);
+            },
+            awsCommand                  : ['extractInstancePrivateIp', function(callback, results){
+                var privateIp = results.extractInstancePrivateIp;
+                var cmd = 'aws ec2 describe-instances --filters Name=private-ip-address,Values='+privateIp+' > /home/ec2-user/instance-data.json';
+                shelljs.exec(cmd, function(code, stderr, stdout){
+                    if ( stderr ) return callback(stderr);
+                    return callback(null, true);
+                });
+            }],
+            extractTags             : ['awsCommand', function(callback, results){
+                fs.readFile('/home/ec2-user/instance-data.json', 'utf8', function (err, data) {
+                    if ( err ) return callback(err);
+                    var obj = JSON.parse(data);
+                    return callback(null, obj.Reservations[0].Instances[0].Tags);
+                });
+            }],
+            setEnviromentVariables  : ['extractTags', function(callback, results){
+                var tags = results.extractTags;
+                async.each(
+                    tags,
+                    function(tag, callback){
+                        process.env[tag.Key] = tag.Value;
+                        callback(null,process.env[tag.Key]);
+                    },
+                    callback
+                );
+            }]
+        }, function(error, results) {
+            if ( error ) return next(err);
+            return next(null, true);
+        });
+    }
+};
+
+module.exports = Controller;
 
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+
 var express = require('express');
 var shelljs = require('shelljs');
+var os = require('os');
 var app = express();
+
+var AWSController = require('deployment/setEnviromentVariables.js');
 
 app.get('/', function (req, res) {
     //res.send(JSON.parse(shelljs.exec('aws ec2 describe-instances --filters Name=tag:NODE_ENV,Values=production')));
-    var privateIp = process.env.HOSTNAME.replace('ip-', '');
-    res.send(JSON.parse(shelljs.exec('aws ec2 describe-instances --filters Name=private-ip-address,Values='+privateIp)));//aws ec2 describe-instances --filters Name=private-ip-address,Values=
-    res.send('Hello World! from AWS!!! UHUHUU');
+    AWSController.setENviromentVariables(function(err, deployment){
+        if ( err ) return res.send(err);
+        if ( deployment ) {
+            res.send(process.env);
+        }
+
+    });
 });
 
 app.listen(3000, function () {
+console.log(os.hostname().replace('ip-',''));
   console.log('Example app listening on port 3000!');
 });
