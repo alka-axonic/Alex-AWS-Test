ad9b80ce4df59fe9feca02b4763f7d51e2214e74
alka-axonic <alexkaefer@gmail.com>
2016-07-21 13:43:55 +0200

    shelljs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var shelljs = requiere('shelljs');
 var app = express();
 
 app.get('/', function (req, res) {
+    res.send(shelljs.exec('aws ec2 describe-tags'));
   res.send('Hello World! from AWS!!! UHUHUU');
 });
 
