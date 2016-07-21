3acaafbd381b67216a69c3ae8c4e03e790bb196e
alka-axonic <alexkaefer@gmail.com>
2016-07-21 14:04:32 +0200

    fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var shelljs = require('shelljs');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send(JSON.parse(shelljs.exec("ec2 describe-instances --filters 'Name=tag:NODE_ENV,Values=production'")));
-  res.send('Hello World! from AWS!!! UHUHUU');
+    res.send(JSON.parse(shelljs.exec('ec2 describe-instances --filters \'Name=tag:NODE_ENV,Values=production\'')));
+    res.send('Hello World! from AWS!!! UHUHUU');
 });
 
 app.listen(3000, function () {
